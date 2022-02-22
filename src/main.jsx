0e05056f3aca4a2aacd6287ad3e4633c5fbd7e5a
Marusiella <67511608+Marusiella@users.noreply.github.com>
2022-02-22 16:18:47 +0100

    clear unnecessary files

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { createGlobalStyle } from 'styled-components'
 import App from './App'
+
+const Global = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #212223;
+  }`;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Global />
   </React.StrictMode>,
   document.getElementById('root')
 )
