b48f43e370c1afb39db03b706a53ea54202f08e6
Marusiella <67511608+Marusiella@users.noreply.github.com>
2022-02-22 16:39:34 +0100

    add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from 'react'
+import styled, { createGlobalStyle } from "styled-components";
+import { useState } from "react";
+const Global = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: ${props => (props.isDark ? "#212223" : "#ECEFF4")};
+  }`;
 
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+const Navbar = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`;
+const Sun = styled.button`
+  color: ${props => (props.isDark ? "#ECEFF4" : "#212223")};
+  font-size: 50px;
+  margin-right: ${props => props.isDark ? "10px" : "17px"};
+  border: none;
+  background-color: transparent;
+`;
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isDark, setIsDark] = useState(false);
   return (
     <div>
-
+      <Global isDark={isDark}/>
+      <MainContainer>
+        <Navbar>
+          <Sun isDark={isDark} onClick={()=>{setIsDark(!isDark);console.log(isDark)}}>{!isDark?"☾":"☼"}</Sun>
+        </Navbar>
+      </MainContainer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createGlobalStyle } from 'styled-components'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Global = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    background-color: #212223;
-  }`;
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Global />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
