9c143f41b998d88a33db5f4c6b5c0b7ecf2736b9
Marusiella <67511608+Marusiella@users.noreply.github.com>
2022-02-22 19:29:45 +0100

    add nodes

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,17 @@ const Sun = styled.button`
   border: none;
   background-color: transparent;
 `;
+const ClockContainer = styled.div`
+  width: 70%;
+  height: 80%;
+  margin: auto;`;
+const Clocks = styled.div`
+  width: calc(100%/3);`;
+const Node = styled.div`
+  width: 10px;
+  height: 10px;
+  background-color: ${props => (props.isDark ? "#ECEFF4" : "#212223")};
+  border-radius: 50%;`;
 function App() {
   const [isDark, setIsDark] = useState(false);
   return (
@@ -34,6 +45,11 @@ function App() {
         <Navbar>
           <Sun isDark={isDark} onClick={()=>{setIsDark(!isDark);console.log(isDark)}}>{!isDark?"☾":"☼"}</Sun>
         </Navbar>
+        <ClockContainer isDark={isDark}>
+          <Clocks isDark={isDark}>
+            <Node isDark={isDark}/>
+          </Clocks>
+        </ClockContainer>
       </MainContainer>
     </div>
   );
