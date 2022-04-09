6f6ae1bd38ef90960f26ac4da7bd8cbafb3d817c
Marusiella <67511608+Marusiella@users.noreply.github.com>
2022-04-09 20:44:10 +0200

    neon effect and improvments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import styled, { createGlobalStyle } from 'styled-components';
-import React, { useEffect, useState } from 'react';
+import styled, { createGlobalStyle, css } from "styled-components";
+import React, { useEffect, useState } from "react";
 
 const Global = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
   body {
     margin: 0;
     padding: 0;
-    background-color: ${(props) => (props.isDark ? '#212223' : '#ECEFF4')};
+    background-color: ${(props) => (props.isDark ? "#212223" : "#ECEFF4")};
   }`;
 
 const MainContainer = styled.div`
@@ -21,9 +21,9 @@ const Navbar = styled.div`
   align-items: center;
 `;
 const Sun = styled.button`
-  color: ${(props) => (props.isDark ? '#ECEFF4' : '#212223')};
+  color: ${(props) => (props.isDark ? "#ECEFF4" : "#212223")};
   font-size: 50px;
-  margin-right: ${(props) => (props.isDark ? '10px' : '17px')};
+  margin-right: ${(props) => (props.isDark ? "10px" : "17px")};
   border: none;
   background-color: transparent;
 `;
@@ -56,9 +56,15 @@ const Clocks = styled.div`
 const Node = styled.div`
   width: 50px;
   height: 50px;
-  background-color: ${(props) => (props.active ? '#F5FF19' : !props.isDark ? '#393939' : '#ECEFF4')};
+  background-color: ${(props) =>
+    props.active ? "#419D78" : !props.isDark ? "#3D4D52" : "#DBD9DB"};
   border-radius: 50%;
-  border: 2px dashed #212223;
+  /* border: 2px solid ${(props) => (!props.isDark ? "#DBD9DB" : "#212223")}; */
+  margin: 2px;
+  ${(props) => props.active == true && css`-webkit-box-shadow: 0px 0px 54px 0px rgba(65, 157, 120, 1);
+-moz-box-shadow: 0px 0px 54px 0px rgba(65, 157, 120, 1);
+box-shadow: 0px 0px 54px 0px rgba(65, 157, 120, 1);`}
+transition: all 0.15s ease-in-out;
 `;
 
 const Text = styled.div`
@@ -66,7 +72,8 @@ const Text = styled.div`
   font-family: "Roboto", sans-serif;
   text-align: center;
   font-weight: bold;
-  color: ${(props) => (props.isDark ? 'white' : '#212223')};`;
+  color: ${(props) => (props.isDark ? "white" : "#3D4D52")};
+`;
 
 function App() {
   const [isDark, setIsDark] = useState(true);
@@ -102,21 +109,21 @@ function App() {
       const secondsBits = date.getSeconds().toString(2);
       setHours(
         hoursBits
-          .padStart(6, '0')
-          .split('')
-          .map((x) => x === '1'),
+          .padStart(6, "0")
+          .split("")
+          .map((x) => x === "1")
       );
       setMinutes(
         minutesBits
-          .padStart(6, '0')
-          .split('')
-          .map((x) => x === '1'),
+          .padStart(6, "0")
+          .split("")
+          .map((x) => x === "1")
       );
       setSeconds(
         secondsBits
-          .padStart(6, '0')
-          .split('')
-          .map((x) => x === '1'),
+          .padStart(6, "0")
+          .split("")
+          .map((x) => x === "1")
       );
       // console.log(hoursBits, minutesBits, secondsBits);
     }, 1000);
@@ -134,30 +141,30 @@ function App() {
               // console.log(isDark);
             }}
           >
-            {!isDark ? '☾' : '☼'}
+            {!isDark ? "☾" : "☼"}
           </Sun>
         </Navbar>
         <ClockContainer isDark={isDark}>
           <div>
             <Clocks isDark={isDark} number={1}>
-              {hours.map((hour, index) => (
-                <Node key={index} isDark={isDark} active={hour} />
+              {hours.map((hour) => (
+                <Node isDark={isDark} active={hour} />
               ))}
             </Clocks>
             <Text isDark={isDark}>Hours</Text>
           </div>
           <div>
             <Clocks isDark={isDark} number={2}>
-              {minutes.map((minute, index) => (
-                <Node key={index} isDark={isDark} active={minute} />
+              {minutes.map((minute) => (
+                <Node isDark={isDark} active={minute} />
               ))}
             </Clocks>
             <Text isDark={isDark}>Minutes</Text>
           </div>
           <div>
             <Clocks isDark={isDark} number={3}>
-              {seconds.map((second, index) => (
-                <Node key={index} isDark={isDark} active={second} />
+              {seconds.map((second) => (
+                <Node isDark={isDark} active={second} />
               ))}
             </Clocks>
             <Text isDark={isDark}>Seconds</Text>
